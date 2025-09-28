5f67278508d314268ed746299d3a2d1bc43899a5
Madhur Sahu <madhursahu@protonmail.com>
2025-09-28 20:13:05 +0530

    [A] HTML node

---
+++ src/test_htmlnode.py
@@ -0,0 +1,14 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class MyTestCase(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode()
+        node2 = HTMLNode()
+        self.assertEqual(node, node2)
+
+
+if __name__ == '__main__':
+    unittest.main()
