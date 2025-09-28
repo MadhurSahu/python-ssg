eda0fe5bc09e78d410fdb23e76fb7e50b42aa0dd
Madhur Sahu <madhursahu@protonmail.com>
2025-09-29 03:47:46 +0530

    [M] Codebase to adapt GitHub pages deploy

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,19 +3,19 @@ import os
 from src.markdown_parser import markdown_to_html_node, extract_title
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_path):
+def generate_pages_recursive(basepath, dir_path_content, template_path, dest_path):
     files = os.listdir(dir_path_content)
     for file in files:
         file_path = os.path.join(dir_path_content, file)
         file_dst_path = os.path.join(dest_path, file.replace(".md", ".html"))
         if os.path.isfile(file_path):
-            generate_page(file_path, template_path, file_dst_path)
+            generate_page(basepath, file_path, template_path, file_dst_path)
         else:
-            generate_pages_recursive(file_path, template_path, file_dst_path)
+            generate_pages_recursive(basepath, file_path, template_path, file_dst_path)
     return
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page form {from_path} to {dest_path} using {template_path}")
     markdown = open(from_path, "r").read()
     template = open(template_path, "r").read()
@@ -23,5 +23,7 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", markdown_html)
+    template = template.replace("href=\"/", f"href=\"{basepath}")
+    template = template.replace("src=\"/", f"src=\"{basepath}")
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     open(dest_path, "w").write(template)
--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
 import os
 import shutil
+import sys
 
 from src.generate_page import generate_pages_recursive
 
 
 def main():
+    basepath = sys.argv[0] if len(sys.argv) == 1 else "/"
     copy_static_data()
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive(basepath, "content", "template.html", "docs")
+
 
 def copy_static_data():
     if not os.path.exists("static"):
         raise Exception("Static data directory doesn't exist")
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
+    os.mkdir("docs")
     copy_static_data_r("")
 
 
@@ -22,7 +25,7 @@ def copy_static_data_r(path):
     files = os.listdir(src)
     for file in files:
         file_path = os.path.join(src, file)
-        dst_path = f"public/{path}/{file}"
+        dst_path = f"docs/{path}/{file}"
         if os.path.isfile(file_path):
             shutil.copy(file_path, dst_path)
         else:
