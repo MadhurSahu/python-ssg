26eb0ff4eed2db21b1d59ad8682dbfc787f3ae7d
Madhur Sahu <madhursahu@protonmail.com>
2025-09-29 02:23:03 +0530

    [A] Static files [M] main.py to copy static files to public on init

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
-from src.textnode import TextNode, TextType
+import os
+import shutil
 
 
 def main():
-    node = TextNode("Hello", TextType.Plain)
-    print(node)
+    copy_static_data()
+
+
+def copy_static_data():
+    if not os.path.exists("static"):
+        raise Exception("Static data directory doesn't exist")
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+    copy_static_data_r("")
+
+
+def copy_static_data_r(path):
+    src = f"static/{path}"
+    files = os.listdir(src)
+    for file in files:
+        file_path = os.path.join(src, file)
+        dst_path = f"public/{path}/{file}"
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, dst_path)
+        else:
+            os.mkdir(dst_path)
+            copy_static_data_r(path + "/" + file)
+    return
 
 
 if __name__ == '__main__':
